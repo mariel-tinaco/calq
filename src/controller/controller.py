fc8261d1440f68197d2de6e4c1607a5002151b68
mariel-tinaco <tinaco.mariel@gmail.com>
2021-12-19 12:19:15 +0800

    Added main function to Controller

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,11 +1,20 @@
 import sys
 import os
-from typing import Protocol
+from PyQt5.QtWidgets import QApplication
+from src.view.view import CalQMainWindow
+from src.controller.utils import Mode
 
 
-class Controller(Protocol):
+class Controller:
 
     def __init__(self) -> None:
         super().__init__()
 
+    
+    def main(self, mode = Mode.GUI):
 
+        if mode == Mode.GUI:
+            app =  QApplication(sys.argv)
+            self.view = CalQMainWindow(self)
+            self.view.show()
+            app.exec()
