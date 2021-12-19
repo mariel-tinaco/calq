f9c56ef3b473d056f45a3177ee4cfa1df19a5fe0
mariel-tinaco <tinaco.mariel@gmail.com>
2021-12-19 12:18:46 +0800

    Added initialization to main window

--- src/view/view.py
+++ src/view/view.py
@@ -1,11 +1,31 @@
 import sys
 import os
 from pathlib import Path
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore, QtGui, uic
+
+UI_PATH = Path("src/view/resources/calq.ui")
 
 
 class CalQMainWindow(QtWidgets.QMainWindow):
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, controller) -> None:
+        super(CalQMainWindow, self).__init__()
+        self.controller = controller
+        uic.loadUi(UI_PATH, self)
 
+        self.initialize()
+
+    def initialize(self):
+
+        self.connect_widgets()
+        self.set_default_states()
+
+    def connect_widgets(self):
+        # # Connect the expression combobox to the expression stack widget
+        self.comboBox_expressions.currentIndexChanged.connect(
+            lambda _ : self.stackedWidget_expr.setCurrentIndex(self.comboBox_expressions.currentIndex())
+        )
+
+    def set_default_states(self):
+        
+        self.stackedWidget_modes.setCurrentIndex(0)
